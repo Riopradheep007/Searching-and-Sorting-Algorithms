9dbe8df759ff6b6024a5d52ed0b5d0fef064c595
Pradheep <46066018+Riopradheep007@users.noreply.github.com>
2021-05-21 20:57:46 +0530

    selection sort

---
+++ Sorting/Selection Sort/selection_sort.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 14:50:08 2021
+
+@author: kpr
+"""
+
+"""
+find the minimum value placed in the left side
+very worst algorithm
+
+Best case O(n^2)
+worst case O(n*2)
+"""
+def selection_sort(ls):
+
+	for i in range(len(ls)):
+	    min_var=i
+	    for j in range(i,len(ls)):
+    		if ls[j]<ls[min_var]:
+    		    min_var=j
+	    ls[i],ls[min_var]=ls[min_var],ls[i]
+	print(ls)
+
+
+ls=[0,7,3,5,2,8,6,1,2]
+selection_sort(ls)
